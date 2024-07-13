a8137b54f9643334b402f6fded1f22c536fcad50
Sandeep992299 <dissanayakesandeep@gmail.com>
2024-07-13 21:50:15 +0530

    database functions Changed

--- backend/database.js
+++ backend/database.js
@@ -121,32 +121,23 @@ const readStudentInfo = async (id) => {
 }
 
 const addStudent = async (id, name, age, religion) => {
-    const sql = `INSERT INTO student(id,name,age,religion) values (?, ?, ?, ?)`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql, [id, name, age, religion])
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
+    try {
+        const data = await knex_db('student').insert({ id, name, age, religion });
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
 
 const updateStudent = async (name, age, religion, id) => {
-    const sql = `UPDATE student SET name=?, age=?, religion=? WHERE id=?`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql, [name, age, religion, id])
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-} 
+    try {
+        const data = await knex_db('student').where({ id }).update({ name, age, religion });
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
 
 const deleteStudent = async (id) => {
     const sql = `DELETE FROM student WHERE id = ?`
